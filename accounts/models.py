be281541f319ab9d609b8ec350c210250d97a2fb
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2026-09-24 23:01:02 +0500

    custom user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        CUSTOMER = "customer", "Customer"
+        PROVIDER = "provider", "Provider"
+
+    role = models.CharField(max_length=10, choices=Role.choices, default=Role.CUSTOMER)
