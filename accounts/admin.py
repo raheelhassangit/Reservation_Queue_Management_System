7d341c07b34bc24457b2c5397b14bba6d350fb80
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2026-09-24 23:05:16 +0500

    custom user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ("Role", {"fields": ("role",)}),
+    )
+    list_display = ("username", "email", "role", "is_staff")
+
 
 admin.site.register(User, UserAdmin)
