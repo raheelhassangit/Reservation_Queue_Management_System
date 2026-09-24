eda3f49dbc4aa1323a91faebf6b0db84467b3192
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2026-09-24 22:44:26 +0500

    static files handling

--- config/settings.py
+++ config/settings.py
@@ -131,15 +131,16 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
-MAILERS = {
-    "default": {
-        "BACKEND": config(
-            "EMAIL_BACKEND",
-            default="django.core.mail.backends.console.EmailBackend",
-        ),
-    },
-}
+EMAIL_BACKEND = config(
+    "EMAIL_BACKEND",
+    default="django.core.mail.backends.console.EmailBackend",
+)
